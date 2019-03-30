88683bb267ec0750b3bc78e3a65f00b9601c6ae4
lawrayette <milhadasdepapel@gmail.com>
2019-03-30 15:42:54 -0600

    foro, test, logo

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 txtName = document.getElementById('txt-name');
 txtTestimony = document.getElementById('txt-testimony');
 shareTestimonyBtn = document.getElementById('share-testimony');
+areaTestimony = document.getElementById('area-testimony');
+emailArea = document.getElementById('email');
 
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
@@ -10,13 +12,19 @@ var db = firebase.firestore();
 const saveTesimony = () => {
     let name = txtName.value;
     let testimony = txtTestimony.value;
+    let area = areaTestimony.value;
+    let email = emailArea.value;
     db.collection("testimonies").add({
         name: name,
         testimony: testimony,
+        area: area,
+        email: email,     
    })
         .then((docRef) => {
             txtName.value = "";
             txtTestimony.value = "";
+            areaTestimony.value = "";
+            emailArea.value = "";
             console.log("Document written with ID: ", docRef.id);
         })
         .catch((error) => {
@@ -30,9 +38,18 @@ db.collection("testimonies").onSnapshot((querySnapshot) => {
     forum.innerHTML= "";
     querySnapshot.forEach(function(doc) {
       forum.innerHTML += `
-      <p>${doc.data().name}</p>
-      <p>${doc.data().testimony}</p>
-     `
+      <div class="row white section">
+        <blockquote >
+        <div class="section">
+        <p class = "flow-text">${doc.data().name}</p>
+        <p>${doc.data().area}</p>
+        <p  class = "section">${doc.data().testimony}</p>
+        </div>
+        </blockquote>
+        <div>
+        <button class = "waves-effect waves-light btn-large"><i class="fas fa-comments"></i></button></div>
+        </div>
+        `
     })
   });
  
